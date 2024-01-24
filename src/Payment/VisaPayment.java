ea6b7f57a66daa9500293ad898838cacb94bedee
Rozi <samakbaevaroza@gmail.com>
2024-01-24 10:07:24 +0600

    Scrum- 63/Feature-Visa Payment

---
+++ src/Payment/VisaPayment.java
@@ -0,0 +1,7 @@
+package Payment;
+
+public class VisaPayment {
+    public void payByVisa(String cardholderName,int cardNumber){
+        System.out.println(cardholderName + "" + cardNumber);
+    }
+}
