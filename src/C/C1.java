e3b4856379492c339572817a8a7001be8d2ba1bd
Rozi <samakbaevaroza@gmail.com>
2024-01-25 10:24:44 +0600

    Feature 7

--- src/C/C1.java
+++ src/C/C1.java
@@ -2,9 +2,6 @@ package C;
 
 public class C1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-    public static  void sayHello(){
-        System.out.println("Hello");
+        System.out.println("Heello");
     }
 }
